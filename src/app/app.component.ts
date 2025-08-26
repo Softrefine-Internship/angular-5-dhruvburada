c22991715137943e6a9b26a8e8ab347b79f2d6f4
dhruvburada <buradadhruv35@gmail.com>
2025-08-26 12:10:22 +0530

    completed Observable Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+import { filter, map, reduce } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [DataService]
 })
-export class AppComponent {
-  title = 'angular-5';
+export class AppComponent implements OnInit {
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.getData().pipe(
+      map((data: number[]) => data.map(value => value * 2).filter(val => val % 2 == 0).reduce((acc, val) => acc + val, 0)),
+    ).subscribe({
+      next: (val) => console.log(`Sum of Even doubled Numbers - ${val}`),
+      complete: () => console.log("Completed"),
+      error: (err) => console.log(err)
+    })
+  }
 }
